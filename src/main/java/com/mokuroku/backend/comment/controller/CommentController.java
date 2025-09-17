5743f56d7c87d9901dc15532166ddad0017f607e
itpark1018 <itpark1018@gmail.com>
2025-09-17 15:24:37 +0900

    (feat/create-comment) 댓글작성 기능을 만들었습니다.
    댓글을 작성하려는 해당 게시글의 식별자를 받아 댓글을 작성합니다.
    댓글 내용과 공개범위를 받습니다.

--- src/main/java/com/mokuroku/backend/comment/controller/CommentController.java
+++ src/main/java/com/mokuroku/backend/comment/controller/CommentController.java
@@ -1,11 +1,15 @@
 package com.mokuroku.backend.comment.controller;
 
+import com.mokuroku.backend.comment.dto.CommentDTO;
 import com.mokuroku.backend.comment.service.CommentService;
 import com.mokuroku.backend.common.ResultDTO;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,9 +19,15 @@ public class CommentController {
   private final CommentService commentService;
 
   @GetMapping("/sns/{postId}/comment")
-  public ResponseEntity<ResultDTO> getComments(@PathVariable Long postId) {
+  public ResponseEntity<ResultDTO<List<CommentDTO>>> getComments(@PathVariable Long postId) {
+    List<CommentDTO> result = commentService.getComment(postId);
+    return ResponseEntity.ok(new ResultDTO<>("댓글 조회에 성공했습니다.", result));
+  }
 
-    ResponseEntity<ResultDTO> result = commentService.getComment(postId);
-    return result;
+  @PostMapping("/sns/{postId}/comment")
+  public ResponseEntity<ResultDTO<CommentDTO>> createComment(@PathVariable Long postId,
+      @RequestBody CommentDTO commentDTO) {
+    CommentDTO result = commentService.createComment(postId, commentDTO);
+    return ResponseEntity.ok(new ResultDTO<>("댓글 작성에 성공했습니다.", result));
   }
 }
